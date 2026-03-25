22b81858a659bba9c997a0e902ad4f9bc2086a98
Кузнецов Николай <nakuznetsov1@stud.eltech.ru>
2026-03-25 21:32:14 +0300

    mouse refactoring

--- src/io/iomouse.h
+++ src/io/iomouse.h
@@ -1,12 +1,8 @@
 #pragma once
 
-#include <QPen>
 #include <QWidget>
 
 #include "iobase.h"
-
-class QSpinBox;
-class QPushButton;
 
 namespace Ripes {
 
@@ -14,27 +10,30 @@ class IOMouse : public IOBase {
   Q_OBJECT
 
   enum Regs { X, Y, LBUTTON, RBUTTON, SCROLL };
+  enum Parameters { WIDTH, HEIGHT };
 
 public:
   IOMouse(QWidget *parent);
-  ~IOMouse() { unregister(); }
+  ~IOMouse() { unregister(); };
 
-  unsigned byteSize() const override { return 5 * 4; }
-  QString description() const override;
-  QString baseName() const override { return "Mouse"; }
+  virtual unsigned byteSize() const override { return 5 * 4; }
+  virtual QString description() const override;
+  virtual QString baseName() const override { return "Mouse"; }
 
-  const std::vector<RegDesc> &registers() const override { return m_regDescs; }
-  const std::vector<IOSymbol> *extraSymbols() const override {
+  virtual const std::vector<RegDesc> &registers() const override {
+    return m_regDescs;
+  };
+  virtual const std::vector<IOSymbol> *extraSymbols() const override {
     return &m_extraSymbols;
   }
 
-  VInt ioRead(AInt offset, unsigned size) override;
-  void ioWrite(AInt offset, VInt value, unsigned size) override;
+  virtual VInt ioRead(AInt offset, unsigned size) override;
+  virtual void ioWrite(AInt offset, VInt value, unsigned size) override;
 
-  void reset() override;
+  virtual void reset() override;
 
 protected:
-  void parameterChanged(unsigned) override {}
+  virtual void parameterChanged(unsigned) override;
 
   void paintEvent(QPaintEvent *event) override;
   void mouseMoveEvent(QMouseEvent *event) override;
@@ -42,28 +41,15 @@ protected:
   void mouseReleaseEvent(QMouseEvent *event) override;
   void wheelEvent(QWheelEvent *event) override;
   QSize minimumSizeHint() const override;
-  QSize sizeHint() const override;
 
 private:
-  void applySize();
-  void repositionWidgets();
+  void updateExtraSymbols();
 
   int m_mouseX = 0;
   int m_mouseY = 0;
   int m_lButton = 0;
   int m_rButton = 0;
-  int m_mButton = 0;
   int m_scroll = 0;
-
-  unsigned m_width = 350;
-  unsigned m_height = 300;
-
-  QSpinBox *m_widthSpin = nullptr;
-  QSpinBox *m_heightSpin = nullptr;
-  QPushButton *m_resetBtn = nullptr;
-
-  static constexpr int kTopH = 30;
-  static constexpr int kBotH = 56;
 
   std::vector<RegDesc> m_regDescs;
   std::vector<IOSymbol> m_extraSymbols;
