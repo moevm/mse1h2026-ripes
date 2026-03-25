5d9a219f8fafe3ea1e25fda0c6447a0ac5787f19
Кузнецов Николай <nakuznetsov1@stud.eltech.ru>
2026-03-25 21:32:06 +0300

    keyboard refactoring

--- src/io/iokeyboard.h
+++ src/io/iokeyboard.h
@@ -1,13 +1,14 @@
 #pragma once
-#include "iobase.h"
-#include <QFrame>
+
 #include <QLabel>
 #include <QMap>
 #include <QMutex>
-#include <QPushButton>
 #include <QQueue>
-#include <QVBoxLayout>
 #include <QWidget>
+
+#include "iobase.h"
+
+QT_FORWARD_DECLARE_CLASS(QPushButton);
 
 namespace Ripes {
 
@@ -18,23 +19,25 @@ class IOKeyboard : public IOBase {
 
 public:
   IOKeyboard(QWidget *parent);
-  ~IOKeyboard() { unregister(); }
+  ~IOKeyboard() { unregister(); };
 
-  unsigned byteSize() const override { return 8; }
-  QString description() const override;
-  QString baseName() const override { return "Keyboard"; }
+  virtual unsigned byteSize() const override { return 8; }
+  virtual QString description() const override;
+  virtual QString baseName() const override { return "Keyboard"; }
 
-  const std::vector<RegDesc> &registers() const override { return m_regDescs; }
-  const std::vector<IOSymbol> *extraSymbols() const override {
+  virtual const std::vector<RegDesc> &registers() const override {
+    return m_regDescs;
+  };
+  virtual const std::vector<IOSymbol> *extraSymbols() const override {
     return &m_extraSymbols;
   }
 
-  VInt ioRead(AInt offset, unsigned size) override;
-  void ioWrite(AInt offset, VInt value, unsigned size) override;
-  void reset() override;
+  virtual VInt ioRead(AInt offset, unsigned size) override;
+  virtual void ioWrite(AInt offset, VInt value, unsigned size) override;
+  virtual void reset() override;
 
 protected:
-  void parameterChanged(unsigned) override;
+  virtual void parameterChanged(unsigned) override;
   void keyPressEvent(QKeyEvent *event) override;
 
 private:
@@ -42,24 +45,12 @@ private:
   void enqueueKey(uint8_t ascii);
   void refreshStatusLabel();
 
-  QHBoxLayout *addKeyRow(QVBoxLayout *parent);
-  QPushButton *createKey(const QString &label, uint8_t ascii, int w = 36,
-                         int h = 36);
-  void flashKey(uint8_t ascii);
-  void clearFlash();
-
   QQueue<uint8_t> m_keyBuffer;
   mutable QMutex m_bufMutex;
   uint8_t m_lastKey = 0;
 
-  QLabel *m_lblData = nullptr;
-  QLabel *m_lblChar = nullptr;
-  QLabel *m_lblFifoCount = nullptr;
-  QMap<uint8_t, QPushButton *> m_keys;
-  QPushButton *m_flashedBtn = nullptr;
-
-  QGridLayout *m_mainLayout = nullptr;
   QLabel *m_statusLabel = nullptr;
+  QMap<uint8_t, QPushButton *> m_keys;
 
   std::vector<RegDesc> m_regDescs;
   std::vector<IOSymbol> m_extraSymbols;
